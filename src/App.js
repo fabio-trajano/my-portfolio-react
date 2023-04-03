d6fbc2f0420617d1b6b90b7c0aede18e3a992573
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-04-03 21:16:45 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
      <Header />
+
+     <main className='home'>
+      <Home />
+     </main>
     </>
   );
 }
