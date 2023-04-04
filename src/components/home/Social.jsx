c496c737353c67499ed34cad8a835ce88740c22e
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-04-05 00:25:45 +0100

    bugfix-href

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,9 +4,9 @@ import "./home.css"
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://github.com/fabio-trajano" className="home__social-icon" target="_blank"><i className="uil uil-github-alt"></i></a>
-      <a href="https://linkedin.com/in/fabio-trajano/" className="home__social-icon" target="_blank"><i className="uil uil-linkedin-alt"></i></a>
-      <a href="https://www.instagram.com/fabiotrajanor/" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
+      <a href="https://github.com/fabio-trajano" rel="noreferrer" className="home__social-icon" target="_blank"><i className="uil uil-github-alt"></i></a>
+      <a href="https://linkedin.com/in/fabio-trajano/" rel="noreferrer" className="home__social-icon" target="_blank"><i className="uil uil-linkedin-alt"></i></a>
+      <a href="https://www.instagram.com/fabiotrajanor/" rel="noreferrer" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
     </div>
   )
 }
