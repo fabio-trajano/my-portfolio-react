c617d182cd01a0986fc16fdbb10d0ea59a640377
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-04-05 14:16:29 +0100

    scrollup and skills

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
-  /* Change Background */
   window.addEventListener("scroll", function () {
     const header = document.querySelector(".header");
-    // when the scroll is higher than 200 viewport height, add the scroll-header class to a tag with the header tag
+
     if (this.scrollY >= 80) header.classList.add("scroll-header");
     else header.classList.remove("scroll-header");
   });
 
-  /*Toggle*/
   const [Toggle, showMenu] = useState(false);
   const [activeNav, setActiveNav] = useState("#home");
 
---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Ruby</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Node.Js</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Python</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Django</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="skills__group">
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">RubyOnRails</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">PostgreSQL</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">SQL</h3>
+              <span className="skills__level"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
