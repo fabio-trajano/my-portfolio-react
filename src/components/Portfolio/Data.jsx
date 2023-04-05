96b6b9ac08aec2ed950307b52640c009309a8c12
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-04-05 17:19:37 +0100

    projects

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -1,8 +1,8 @@
 import Work1 from "../../assets/project1.png";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work2 from "../../assets/project2.png";
+import Work3 from "../../assets/project3.png";
+import Work4 from "../../assets/project4.png";
+
 
 export const projectsData = [
   {
@@ -15,37 +15,37 @@ export const projectsData = [
   {
     id: 2,
     image: Work2,
-    title: "App movil",
-    category: "React",
+    title: "Petbnb",
+    category: "Rails",
     url: "https://www.github.com/fabio-trajno/"
   },
   {
     id: 3,
     image: Work3,
-    title: "Brand design",
-    category: "Rails",
+    title: "Calculator",
+    category: "React",
     url: "https://www.github.com/fabio-trajno/"
   },
   {
     id: 4,
     image: Work4,
-    title: "App movil",
-    category: "Rails",
+    title: "Real-time Weather",
+    category: "Others",
     url: "https://www.github.com/fabio-trajno/"
   },
-  {
-    id: 5,
-    image: Work5,
-    title: "Web design",
-    category: "Rails",
-    url: "https://www.github.com/fabio-trajno/"
-  },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: "",
+  //   category: "Rails",
+  //   url: "https://www.github.com/fabio-trajno/"
+  // },
 ];
 
 // projects
 export const projectsNav = [
   {
-    name: "all",
+    name: "All",
   },
   {
     name: "Rails",
