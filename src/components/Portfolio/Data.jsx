196bb01ec26c5170db9e66025236ee4180b8f5d6
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-05-22 16:39:59 +0100

    added new project (Django API)

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -1,44 +1,47 @@
 import Work1 from "../../assets/project1.png";
 import Work2 from "../../assets/project2.png";
 import Work3 from "../../assets/project3.png";
-import Work4 from "../../assets/project4.png";
-
+import Work5 from "../../assets/project5.png";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
+    linktext: "Demo",
     title: "PocketSommelier",
     category: "Rails",
     url: "https://www.pocketsommelier.me/"
   },
   {
+    id: 5,
+    image: Work5,
+    title: "Restaurant API - Django(DRF)",
+    linktext: "Source Code",
+    category: "Python/Django",
+    url: "https://github.com/fabio-trajano/Meta-backend-capstone-project/tree/master/MetaCapstoneProject"
+  },
+  {
     id: 2,
     image: Work2,
+    linktext: "Source Code",
     title: "Petbnb",
-    category: "Rails",
-    url: "https://www.github.com/fabio-trajno/"
+    category: "Ruby on Rails",
+    url: "https://github.com/AndreNelas93/petbnb"
   },
   {
     id: 3,
     image: Work3,
     title: "Calculator",
+    linktext: "Source Code",
     category: "React",
-    url: "https://www.github.com/fabio-trajno/"
-  },
-  {
-    id: 4,
-    image: Work4,
-    title: "Real-time Weather",
-    category: "Others",
-    url: "https://www.github.com/fabio-trajno/"
+    url: "https://github.com/fabio-trajano/react-calculators/tree/master"
   },
   // {
-  //   id: 5,
-  //   image: Work5,
-  //   title: "",
-  //   category: "Rails",
-  //   url: "https://www.github.com/fabio-trajno/"
+  //   id: 4,
+  //   image: Work4,
+  //   title: "Real-time Weather",
+  //   category: "Others",
+  //   url: "https://www.github.com/fabio-trajano/"
   // },
 ];
 
@@ -48,12 +51,12 @@ export const projectsNav = [
     name: "All",
   },
   {
-    name: "Rails",
+    name: "Ruby on Rails",
   },
   {
     name: "React",
   },
   {
-    name: "Others",
+    name: "Python/Django",
   },
 ];
