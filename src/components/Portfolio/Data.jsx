f496272ef822fec41e73f43c7843691b41665a9e
Fabio Trajano Ramalho <114175707+fabio-trajano@users.noreply.github.com>
2024-12-03 18:30:24 +0000

    add outfit size finder project

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -4,6 +4,7 @@ import Work3 from "../../assets/project3.png";
 import Work5 from "../../assets/project5.png";
 import Work6 from "../../assets/project6.png";
 import Work7 from "../../assets/project7.png";
+import Work8 from "../../assets/project8.png";
 
 export const projectsData = [
 
@@ -24,7 +25,15 @@ export const projectsData = [
     url: "https://github.com/AndreNelas93/pocketsommelier"
   },
   {
-     id: 3,
+    id: 3,
+    image: Work8,
+    linktext: "Source Code",
+    title: "Outfit Size Recommender - Python/ML",
+    category: "Machine Learning and AI",
+    url: "https://github.com/fabio-trajano/outfit-size-finder"
+ },
+  {
+     id: 4,
      image: Work6,
      linktext: "Source Code",
      title: "Chess Backend - Java/SpringBoot",
@@ -32,7 +41,7 @@ export const projectsData = [
      url: "https://github.com/fabio-trajano/chess-java"
   },
   {
-    id: 4,
+    id: 5,
     image: Work7,
     linktext: "Source Code",
     title: "Ticker Analysis - Java",
@@ -40,7 +49,7 @@ export const projectsData = [
     url: "https://github.com/fabio-trajano/java-financial-analysis"
 },
   {
-    id: 5,
+    id: 6,
     image: Work5,
     title: "Restaurant API - Python/Django",
     linktext: "Source Code",
@@ -48,7 +57,7 @@ export const projectsData = [
     url: "https://github.com/fabio-trajano/Meta-backend-capstone-project/tree/master/MetaCapstoneProject"
   },
   {
-    id: 6,
+    id: 7,
     image: Work2,
     linktext: "Source Code",
     title: "Petbnb RubyOnRails",
