b2f905e17bbb5c7f7cc98fc042acf5a348b76937
Fabio Trajano Ramalho <114175707+fabio-trajano@users.noreply.github.com>
2024-12-03 18:23:06 +0000

    add ml project

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -6,16 +6,25 @@ import Work6 from "../../assets/project6.png";
 import Work7 from "../../assets/project7.png";
 
 export const projectsData = [
+
   {
     id: 1,
+    image: Work3,
+    title: "Sports Odds Predictor - Python/ML",
+    linktext: "Source Code",
+    category: "Machine Learning and AI",
+    url: "https://github.com/fabio-trajano/sports-odds-predictor"
+  },
+  {
+    id: 2,
     image: Work1,
     linktext: "Source Code",
-    title: "PocketSommelier - Ruby on Rails",
-    category: "Full Stack",
+    title: "PocketSommelier - RubyOnRails",
+    category: "Others",
     url: "https://github.com/AndreNelas93/pocketsommelier"
   },
   {
-     id: 2,
+     id: 3,
      image: Work6,
      linktext: "Source Code",
      title: "Chess Backend - Java/SpringBoot",
@@ -23,15 +32,15 @@ export const projectsData = [
      url: "https://github.com/fabio-trajano/chess-java"
   },
   {
-    id: 3,
+    id: 4,
     image: Work7,
     linktext: "Source Code",
-    title: "Financial Analysis API - Java/SpringBoot",
+    title: "Ticker Analysis - Java",
     category: "Back End",
     url: "https://github.com/fabio-trajano/java-financial-analysis"
 },
   {
-    id: 4,
+    id: 5,
     image: Work5,
     title: "Restaurant API - Python/Django",
     linktext: "Source Code",
@@ -39,20 +48,12 @@ export const projectsData = [
     url: "https://github.com/fabio-trajano/Meta-backend-capstone-project/tree/master/MetaCapstoneProject"
   },
   {
-    id: 5,
+    id: 6,
     image: Work2,
     linktext: "Source Code",
-    title: "Petbnb - Ruby on Rails",
-    category: "Full Stack",
+    title: "Petbnb RubyOnRails",
+    category: "Others",
     url: "https://github.com/AndreNelas93/petbnb"
-  },
-  {
-    id: 6,
-    image: Work3,
-    title: "Basic Calculator - React",
-    linktext: "Source Code",
-    category: "Full Stack",
-    url: "https://github.com/fabio-trajano/react-calculators/tree/master"
   },
 ];
 
@@ -62,9 +63,12 @@ export const projectsNav = [
     name: "All",
   },
   {
-      name: "Back End",
+    name: "Back End",
   },
   {
-    name: "Full Stack",
+    name: "Machine Learning and AI",
+  },
+  {
+    name: "Others",
   },
 ];
